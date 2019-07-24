9d31cf0f51a02f9b9bc8a931ac25c9aa9204705e
Maliha Taufiq <maliha.taufiq@verizon.com>
2019-07-24 16:30:59 -0500

    update1

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+//Setting up express
 const express = require('express');
-
+const data = require('./data.json');
 const app = express();
 
+//Setting up middleware
+// app.use(bodyParser.urlencoded({ extended: false}));
 
-app.get('/', (request, response) => {
-    response.send('I love treehouse!');
+//Setting up pug
+//Update code in app to use Pug
+app.set('view engine', 'pug');
+
+//Merges the data with the templates to surf dynamic pages.
+app.get('/', (req, res) => {
+    res.render('index', {projects: data.projects});
 });
 
-app.listen(3000);
+//Adding routes and sending strings to the client.
+//What will appear in the home section.
+// app.get('/home', (req, res) => {
+//     res.render('<h1>Home</h1>');
+// });
+
+//What will appear in the about section.
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+//When nodemon restarts the server there’s a message.
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!');
+});
 
