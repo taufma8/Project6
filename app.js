fed4e989459e3addbd12b10c47d0dad6cddb23de
Maliha Taufiq <maliha.taufiq@verizon.com>
2019-07-25 17:08:08 -0500

    updated everything

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
 //Setting up express
 const express = require('express');
 const data = require('./data.json');
+const bodyParser = require('body-parser');
 const app = express();
 
 //Setting up middleware
-// app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //Setting up pug
 //Update code in app to use Pug
 app.set('view engine', 'pug');
 
+//Using a static route and the express.static method to serve the static files located in the public folder.
+app.use('/static', express.static('public'));
+app.use('/images', express.static('images'));
+
+
+//Adding routes and sending strings to the client.
+
 //Merges the data with the templates to surf dynamic pages.
+//What will appear in the home section.
 app.get('/', (req, res) => {
     res.render('index', {projects: data.projects});
 });
 
-//Adding routes and sending strings to the client.
-//What will appear in the home section.
-// app.get('/home', (req, res) => {
-//     res.render('<h1>Home</h1>');
-// });
-
 //What will appear in the about section.
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+//Dynamic routes
+app.get('/projects/:id', (req, res) => {
+    if (req.params.id > data.projects.length || req.params.id <  1) {
+        const err = new Error('This page cannot be found.');
+        err.status = 404;
+        throw err;
+    } 
+    res.render('project', {
+        project: data.projects[req.params.id - 1],
+        id: req.params.id  
+    });
+});
+
+//Handling errors: 404
+app.use((req, res, next) => {
+    console.log('This page does not exist.');
+    const err = new Error('This page cannot be found.');
+    err.status = 404;
+    next(err);
+});
+
+//Handling errors
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 //When nodemon restarts the server there’s a message.
